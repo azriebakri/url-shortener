bb44aad70d2137776703f34b30995383f402e996
Azrie <mohamad.azriebakri@gmail.com>
2020-02-02 03:20:56 +0800

    Added firebase configuration

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
